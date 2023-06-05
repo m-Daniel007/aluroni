8f520bb3ef488f4f1dd279cd0fd83df0b3a3c748
Marcos <marcosdanieldossantos@yahoo.com.br>
2023-06-05 12:11:01 -0300

    iniciando pag. nao encontrada

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,15 +1,15 @@
-import styles from './Cardapio.module.scss';
-import Buscador from './Buscador';
-import { useState } from 'react';
-import Filtros from './Filtros';
-import Ordenador from './Ordenador';
-import Itens from './Itens';
-import stylesTema from 'styles/Tema.module.scss'
+import styles from "./Cardapio.module.scss";
+import Buscador from "./Buscador";
+import { useState } from "react";
+import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
+import Itens from "./Itens";
+import stylesTema from "styles/Tema.module.scss";
 
 export default function Cardapio() {
-  const [busca, setBusca] = useState('');
+  const [busca, setBusca] = useState("");
   const [filtro, setFiltro] = useState<number | null>(null);
-  const [ordenador, setOrdenador] = useState('');
+  const [ordenador, setOrdenador] = useState("");
   return (
     <section className={styles.cardapio}>
       <h3 className={stylesTema.titulo}>Cardápio</h3>
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
+import Footer from 'components/Footer';
 import Menu from 'components/Menu';
 import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
@@ -13,8 +15,10 @@ export default function AppRouter() {
           <Route path='/' element={<PaginaPadrao />} >
             <Route index element={<Inicio />} />
             <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre/>}/>
           </Route>
         </Routes>
+        <Footer />
       </Router>
     </main>
   );
