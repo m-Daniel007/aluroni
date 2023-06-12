d87db65675d8b16c8cef191a4b62285f35264376
Marcos <marcosdanieldossantos@yahoo.com.br>
2023-06-12 18:11:44 -0300

    refatorando o header e ajustes finais

--- src/components/PaginaPadrao/index.tsx
+++ src/components/PaginaPadrao/index.tsx
@@ -1,17 +1,21 @@
-import styles from './PaginaPadrao.module.scss';
-import { Outlet } from 'react-router-dom';
-import stylesTema from 'styles/Tema.module.scss'
+import React from "react";
+import styles from "./PaginaPadrao.module.scss";
+import { Outlet } from "react-router-dom";
+import stylesTema from "styles/Tema.module.scss";
 
-export default function PaginaPadrao() {
+export default function PaginaPadrao({
+  children,
+}: {
+  children?: React.ReactNode;
+}) {
   return (
     <>
       <header className={styles.header}>
-        <div className={styles.header__text}>
-          A casa do código e da massa
-        </div>
+        <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
       <div className={stylesTema.container}>
         <Outlet />
+        {children}
       </div>
     </>
   );
